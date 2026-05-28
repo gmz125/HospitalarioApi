112146f300bcef8f23099d419d37f30db7a2bed4
GMZ125 <222t0239@itsm.edu.mx>
2026-05-28 13:47:15 -0600

    ventasss

--- HospitalarioApi/Models/Venta.cs
+++ HospitalarioApi/Models/Venta.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace HospitalarioApi.Models
 {
@@ -6,18 +7,23 @@ namespace HospitalarioApi.Models
     public class Venta
     {
         [Column("id")]
+        [JsonPropertyName("id")]
         public Guid Id { get; set; }
 
         [Column("cliente_id")]
+        [JsonPropertyName("cliente_id")]
         public string? ClienteId { get; set; }
 
         [Column("total")]
+        [JsonPropertyName("total")]
         public decimal Total { get; set; }
 
         [Column("nombre_negocio")]
-        public string NombreNegocio { get; set; } = string.Empty;
+        [JsonPropertyName("nombre_negocio")]
+        public string? NombreNegocio { get; set; }
 
         [Column("fecha")]
-        public DateTime Fecha { get; set; } = DateTime.UtcNow;
+        [JsonPropertyName("fecha")]
+        public DateTime Fecha { get; set; }
     }
 }
