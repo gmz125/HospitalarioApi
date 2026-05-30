31df481277cf584162fb8b3f551f53823dc3fff4
GMZ125 <222t0239@itsm.edu.mx>
2026-05-30 14:58:49 -0600

    optimizamos ventas

--- HospitalarioApi/Models/Venta.cs
+++ HospitalarioApi/Models/Venta.cs
@@ -14,16 +14,28 @@ namespace HospitalarioApi.Models
         [JsonPropertyName("cliente_id")]
         public string? ClienteId { get; set; }
 
+        [Column("medicamento_id")]
+        [JsonPropertyName("medicamento_id")]
+        public Guid? MedicamentoId { get; set; }
+
+        [Column("nombre_medicamento")]
+        [JsonPropertyName("nombre_medicamento")]
+        public string? NombreMedicamento { get; set; }
+
+        [Column("cantidad")]
+        [JsonPropertyName("cantidad")]
+        public int Cantidad { get; set; }
+
         [Column("total")]
         [JsonPropertyName("total")]
         public decimal Total { get; set; }
 
         [Column("nombre_negocio")]
         [JsonPropertyName("nombre_negocio")]
-        public string? NombreNegocio { get; set; }
+        public string NombreNegocio { get; set; } = string.Empty;
 
         [Column("fecha")]
         [JsonPropertyName("fecha")]
-        public DateTime Fecha { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.UtcNow;
     }
 }
